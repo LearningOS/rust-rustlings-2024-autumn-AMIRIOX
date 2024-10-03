6190b7de692df47b45af228ee7f27224a3de7562
AMIRIOX <wumingyun2120@outlook.com>
2024-10-03 21:16:09 +0800

    hashmaps 1 2 3, quiz 2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,14 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let iter1 = scores.entry(team_1_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        (*iter1).goals_scored += team_1_score;
+        (*iter1).goals_conceded += team_2_score;
+       
+        let iter2 = scores.entry(team_2_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        (*iter2).goals_scored += team_2_score;
+        (*iter2).goals_conceded += team_1_score;
+
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,28 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    let mut str2 = string.clone();
+                    str2.make_ascii_uppercase();
+                    output.push(str2);
+                },
+                Command::Trim => {
+                    output.push(string.trim_start().trim_end().to_string());
+                },
+                Command::Append(u) => {
+                    let mut str2 = string.clone();
+                    for _ in 1..=*u as i32 {
+                        str2 += "bar";
+                    }
+                    output.push(str2);
+                }
+            }
         }
         output
     }
@@ -45,7 +62,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
